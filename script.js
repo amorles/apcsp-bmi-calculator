546bd8f0a506c20b6927f57435347d813d72d588
amorles <54360375+amorles@users.noreply.github.com>
2020-02-24 13:46:33 -0500

    done js

--- script.js
+++ script.js
@@ -21,19 +21,33 @@ function bmiCalculation(){
 function checkBMI(){
     if(bmi<18.5){
         classification = 1
-        document.getElementById("results").innerHTML = "You are underweight! Your BMI is: " + bmi + ". ;
+        document.getElementById("results").innerHTML = "You are underweight according to the BMI scale! Your current BMI is: " + parseFloat(bmi).toFixed(2) +". Healthy weight begins at a BMI score of 18.5.";
+        document.getElementById("results").classList.remove('underweight', 'healthy', 'overweight', 'obese', 'eobese')
+        document.getElementById("results").classList.add('underweight')
     }
     else if(bmi<25){
         classification = 2
+        document.getElementById("results").innerHTML = "You are perfectly healthy according to the BMI scale! Your current BMI is: " + parseFloat(bmi).toFixed(2) +". Underweight ends at a BMI score of 18.49. and overweight begins at 25.";
+        document.getElementById("results").classList.remove('underweight', 'healthy', 'overweight', 'obese', 'eobese')
+        document.getElementById("results").classList.add('healthy')
     }
     else if(bmi<30){
         classification = 3
+        document.getElementById("results").innerHTML = "You are overweight according to the BMI scale! Your current BMI is: " + parseFloat(bmi).toFixed(2) +". Healthy weight ends at a BMI score of 24.99 and obese begins at 30.";
+        document.getElementById("results").classList.remove('underweight', 'healthy', 'overweight', 'obese', 'eobese')
+        document.getElementById("results").classList.add('overweight')
     }
     else if(bmi<35){
         classification = 4
+        document.getElementById("results").innerHTML = "You are obese according to the BMI scale! Your current BMI is: " + parseFloat(bmi).toFixed(2) +". Overweight ends at a BMI score of 29.99 and extremely obese begins at 35.";
+        document.getElementById("results").classList.remove('underweight', 'healthy', 'overweight', 'obese', 'eobese')
+        document.getElementById("results").classList.add('obese')
     }
     else if(bmi>=35){
         classification = 5
+        document.getElementById("results").innerHTML = "You are extremely obese according to the BMI scale! Your current BMI is: " + parseFloat(bmi).toFixed(2) +". Obese ends at a BMI score of 34.99.";
+        document.getElementById("results").classList.remove('underweight', 'healthy', 'overweight', 'obese', 'eobese')
+        document.getElementById("results").classList.add('eobese')
     }
     else{
         classification = 6
